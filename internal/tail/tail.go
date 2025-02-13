9d4d7260d8fd89bbc6a3c8a067722707c22d8f1a
Chris Gavin <chris@chrisgavin.me>
2025-02-13 20:36:20 +0000

    Implement a custom tail system to fix bugs in the library.

---
+++ internal/tail/tail.go
@@ -0,0 +1,86 @@
+package tail
+
+import (
+	"bufio"
+	"io"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/pkg/errors"
+)
+
+const pollFrequency = 100 * time.Millisecond
+
+type Line struct {
+	Text string
+	Err  error
+}
+
+type Tailer struct {
+	handle   *os.File
+	Lines    chan Line
+	shutdown bool
+}
+
+type Options struct {
+	NewLinesOnly bool
+	Truncate     bool
+}
+
+func TailFile(path string, options Options) (*Tailer, error) {
+	if options.Truncate {
+		err := os.Truncate(path, 0)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to truncate file")
+		}
+	}
+
+	handle, err := os.Open(path)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to open file")
+	}
+	if options.NewLinesOnly {
+		_, err := handle.Seek(0, io.SeekEnd)
+		if err != nil {
+			return nil, errors.Wrap(err, "failed to seek to end of file")
+		}
+	}
+	tailer := &Tailer{
+		handle: handle,
+		Lines:  make(chan Line),
+	}
+	tailer.Start()
+	return tailer, nil
+}
+
+func (t *Tailer) Start() {
+	reader := bufio.NewReader(t.handle)
+	go func() {
+		for {
+			line, err := reader.ReadString('\n')
+			if err != nil && !errors.Is(err, io.EOF) {
+				t.Lines <- Line{Err: err}
+				return
+			}
+			if errors.Is(err, io.EOF) {
+				if t.shutdown {
+					close(t.Lines)
+					return
+				}
+				time.Sleep(pollFrequency)
+				continue
+			}
+			line = strings.TrimSuffix(line, "\n")
+			t.Lines <- Line{Text: line}
+		}
+	}()
+}
+
+func (t *Tailer) StopAtEOF() {
+	t.shutdown = true
+}
+
+func (t *Tailer) Cleanup() error {
+	return errors.Wrap(t.handle.Close(), "failed to close file")
+}
